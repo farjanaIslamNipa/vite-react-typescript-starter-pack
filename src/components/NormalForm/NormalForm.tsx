44174c688dd6e065c9e95b18c439fc8be8fabb19
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-01-15 21:56:58 +0600

    working on forms

--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import cn from '../../utils/cn';
 
 const NormalForm = () => {
   const { register, handleSubmit } = useForm()
@@ -6,13 +7,31 @@ const NormalForm = () => {
   const onSubmit = (data) => {
     console.log(data)
   }
+
+  const double = true
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input className='border border-gray-500 rounded-sm' type="text" id="name" {...register('name')} />
+    <form onSubmit={handleSubmit(onSubmit)} className={cn('border border-red-400 w-full max-w-5xl p-5 mx-auto', {
+      'max-w-5xl' : double,
+      'max-w-md' : !double
+    })}>
+      <div className={cn('grid grid-cols-1 gap-5', {
+        'md:grid-cols-2' : double
+      })}>
+        <div >
+          <label className='block' htmlFor="name">Name</label>
+          <input className='w-full' type="text" id="name" {...register('name')} />
+        </div>
+        <div>
+          <label className='block' htmlFor="email">Email</label>
+          <input className='w-full' type="text" id="email" {...register('email')} />
+        </div>
+        <div>
+          <label className='block' htmlFor="password">Password</label>
+          <input className='w-full' type="text" id="password" {...register('password')} />
+        </div>
+        <button type='submit'>Submit</button>
       </div>
-      <button type='submit'>Submit</button>
     </form>
   );
 };
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -43,8 +43,8 @@ const CloseButton = ({ children } : TCloseButton) => {
   const { onClose } = useContext(ModalContext) as TModalContext
   return <button onClick={ onClose }>
     {children ? children : (
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
     </svg>
     )}
   </button>
